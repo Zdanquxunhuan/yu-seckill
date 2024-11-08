39ff9b26ee3ec56e03d924c50db73fab19ee22ef
zhongcanyu <zcyzcy159162>
2024-11-08 11:30:53 +0800

    feat: base utils

---
+++ seckill-common/src/main/java/yu/seckill/common/utils/CommonUtils.java
@@ -0,0 +1,44 @@
+package yu.seckill.common.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.CollectionUtils;
+import yu.seckill.common.exception.SeckillException;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * @Author: Administrator
+ * @CreateTime: 2024-11-08
+ * @Description:
+ */
+public class CommonUtils {
+
+    public static <T> T requireNonNull(T obj, String msg) {
+
+        if (obj == null) {
+            throw new SeckillException(msg);
+        }
+
+        if (obj instanceof String) {
+            if (StringUtils.isBlank((String) obj)) {
+                throw new SeckillException(msg);
+            }
+        }
+
+        if (obj instanceof Collections) {
+            if (CollectionUtils.isEmpty((Collection<?>) obj)) {
+                throw new SeckillException(msg);
+            }
+        }
+
+        if (obj instanceof Map) {
+            if (MapUtils.isEmpty((Map<?, ?>) obj)) {
+                throw new SeckillException(msg);
+            }
+        }
+
+        return obj;
+    }
+}
---
+++ seckill-common/src/main/java/yu/seckill/common/utils/MapUtils.java
@@ -0,0 +1,15 @@
+package yu.seckill.common.utils;
+
+import java.util.Map;
+
+/**
+ * @Author: Administrator
+ * @CreateTime: 2024-11-08
+ * @Description:
+ */
+public class MapUtils {
+
+    public static boolean isEmpty(Map<?, ?> map) {
+        return map == null || map.isEmpty();
+    }
+}
